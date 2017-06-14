a50f45531e91a8cb5b7b4f19c6474928f52313b5
Filip Trajkovski <trajkovski@its-sk.com.mk>
2017-06-14 13:36:28 +0200

    First commite

---
+++ src/main/java/com/filip/Client3Application.java
@@ -0,0 +1,12 @@
+package com.filip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Client3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Client3Application.class, args);
+	}
+}
